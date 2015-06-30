91a8660d810745b60026123cb0cdaf7bc32c7537
Leo G <leo@leog.in>
2015-06-30 21:29:03 +0530

    hmac

--- github-webhook.py
+++ github-webhook.py
@@ -1,11 +1,25 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
+#http://pymotw.com/2/hmac/
+import hmac
+import hashlib
+#http://techarena51.com/index.php/how-to-install-python-3-and-flask-on-linux/
 import subprocess
 
 app = Flask(__name__)
 
+
+def verify_hmac_hash(data, key)
+    digest_maker = hmac.new('some secret', data, hashlib.sha1)
+    digest = digest_maker.digest()   
+    
+    return hmac.compare_digest(digest, key)
+    
+    
+
 @app.route("/payload")
 def github_payload():      
       if request.headers.get('X-GitHub-Event') == "ping":
+        Sig = request.headers.get('X-GitHub-Event'
         print(request.get_json())
         return jsonify({'msg': 'Ok'})
       if request.headers.get('X-GitHub-Event') == "push":
