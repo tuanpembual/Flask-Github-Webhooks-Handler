d247cd7d51d6262dc2ff6d65d8b5fece7b3e7e6f
Leo G <leo@leog.in>
2015-06-30 18:56:21 +0530

    init

---
+++ github-webhook.py
@@ -0,0 +1,19 @@
+from flask import render_template, request, redirect, url_for, jsonify
+import subprocess
+
+app = Flask(__name__)
+
+@app.route("/payload")
+def github_payload():      
+      if request.headers.get('X-GitHub-Event') == "ping":
+        print(request.get_json())
+        return jsonify({'msg': 'Ok'})
+      if request.headers.get('X-GitHub-Event') == "push":
+          payload = request.get_json()
+          if  payload['commits'][0]['distinct'] == True:
+              cmd = subprocess.Popen(['bash','/sites/github-webhooks/git.bash'],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+              out,error = cmd.communicate()                 
+              return jsonify({'msg': 'successfully ran git pull'})
+
+if __name__ == "__main__":
+    app.run()
