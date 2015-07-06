d74e81d4cf6556bb4a33f9ec4b5872f4fcfead6b
Leo <leo@leog.in>
2015-07-07 01:11:33 +0530

    remove ip and auto-pep8

--- github-webhook.py
+++ github-webhook.py
@@ -1,15 +1,15 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
-#http://pymotw.com/2/hmac/
+# http://pymotw.com/2/hmac/
 import hmac
 import hashlib
-#http://techarena51.com/index.php/how-to-install-python-3-and-flask-on-linux/
+# http://techarena51.com/index.php/how-to-install-python-3-and-flask-on-linux/
 import subprocess
 
 app = Flask(__name__)
 
 
 def verify_hmac_hash(data, signature):
-    github_secret = bytes('some secret', 'UTF-8');
+    github_secret = bytes('some secret', 'UTF-8')
     mac = hmac.new(github_secret, msg=data, digestmod=hashlib.sha1)
     return hmac.compare_digest('sha1=' + mac.hexdigest(), signature)
 
@@ -19,16 +19,17 @@ def github_payload():
     signature = request.headers.get('X-Hub-Signature')
     data = request.data
     if verify_hmac_hash(data, signature):
-      if request.headers.get('X-GitHub-Event') == "ping":
-        return jsonify({'msg': 'Ok'})
-      if request.headers.get('X-GitHub-Event') == "push":
-          payload = request.get_json()
-          if  payload['commits'][0]['distinct'] == True:
-              try:
-                  cmd_output=subprocess.check_output(['git', 'pull', 'origin', 'master' ],)
-                  return jsonify({'msg': str(cmd_output)})
-              except subprocess.CalledProcessError as error: 
-                  return jsonify({'msg': str(error.output)})                  
+        if request.headers.get('X-GitHub-Event') == "ping":
+            return jsonify({'msg': 'Ok'})
+        if request.headers.get('X-GitHub-Event') == "push":
+            payload = request.get_json()
+            if payload['commits'][0]['distinct'] == True:
+                try:
+                    cmd_output = subprocess.check_output(
+                        ['git', 'pull', 'origin', 'master'],)
+                    return jsonify({'msg': str(cmd_output)})
+                except subprocess.CalledProcessError as error:
+                    return jsonify({'msg': str(error.output)})
 
     else:
         return jsonify({'msg': 'invalid hash'})
@@ -36,4 +37,4 @@ def github_payload():
 
 if __name__ == "__main__":
     app.debug = True
-    app.run(host="23.250.18.72")
+    app.run(host="127.0.0.1")
